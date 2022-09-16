77d90fdb38a84c5621156f8b8d4151cbf2ce3a62
Kyoko <booornb@gmail.com>
2022-09-16 10:56:04 +0900

    Updated Style and Video setting

--- src/work/facyapp.php
+++ src/work/facyapp.php
@@ -66,7 +66,7 @@
 
    <figure>
       <div class="video small">
-         <video src="../image/app_User_Test.mp4" loop autoplay muted playsinline></video>
+         <video src="../image/app_User_Test.mp4" type="video/mp4" autoplay loop muted playsinline></video>
       </div>
       <figcaption>
          <p>
